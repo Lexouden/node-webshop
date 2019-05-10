85ebcb25f88f9f8edc80de1df08c722529c510d3
Lexouden <99044652@mydavinci.nl>
2019-05-10 14:22:21 +0200

    Login element
    Started custom login element

--- public/src/components/login.js
+++ public/src/components/login.js
@@ -1,4 +1,53 @@
 import {
-  html
+  LitElement,
+  html,
+  css
+} from 'lit-element';
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-material/paper-material.js';
+import '@polymer/paper-button/paper-button.js';
+
+
+class LoginElement extends LitElement {
+  static get styles() {
+    return css `
+      :host {
+        display: block;
+        max-height: 600px;
+        max-width: 400px;
+        margin: 0 auto;
+      }
+
+      .blue {
+        background-color: white;
+      }
+
+      .login-input {
+        color: white;
+      }
+    `
+  }
+
+  render() {
+    return html `
+      <paper-material elevation="1">
+        <div>
+          <paper-input class="login-input" id="username" placeholder="Username"></paper-input>
+        </div>
+        
+        <div>
+          <paper-input class="login-input" id="password" type="password" placeholder="Password"></paper-input>
+        </div>
+
+        <div>
+          <paper-button raised class="blue">Login</paper-button>
+        </div>
+
+      </paper-material>
+        
+
+    `
+  }
 }
-from "lit-html";
+
+customElements.define('login-element', LoginElement);
--- public/src/components/shop.js
+++ public/src/components/shop.js
@@ -1,7 +1,35 @@
 import {
   html
-}
-from "lit-html";
+} from 'lit-html';
 
-export const Webshop = () => html `
-  `
+export const Webshop_Template = () => html `
+  <nav>
+    <div class="mini">
+      <p class="products">Empty</p>
+      <p class="names"></p>
+      <p class="miniprice"></p>
+    </div>
+    <ul class="navbar-nav">
+      <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+    </ul>
+
+    <div class="cart fa fa-shopping-cart">
+      <span class="number">1</span>
+    </div>
+  </nav>
+  <div class="container" id="categories">
+
+  </div>
+  <div class="container" id="products">
+
+  </div>
+  <div class="quickviewContainer">
+    <div class="close"></div>
+    <h2 class="headline"></h2>
+    <p class="description"></p>
+    <img src="" alt="">
+    <img src="" alt="">
+    <img src="" alt="">
+  </div>
+  </div>
+`
