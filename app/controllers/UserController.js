fbadde5e73b17ac3589848ccf49aec4220390474
Lexouden <99044652@mydavinci.nl>
2019-05-21 14:28:12 +0200

    Update UserController.js
    updated the login function
    added a remove and edit function

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,19 +1,24 @@
 const User = require('../models/User');
 
-exports.login = (user, pass) => {
+exports.login = ({
+  username,
+  password
+}, callback) => {
   User.findOne({
-    username: user
+    username: username
   }, (err, user) => {
     if (err) return console.error('An error occurred while retrieving user:', `\n${err}`);
-    if (user === null) return console.error('User not found.')
-    User.comparePassword(pass, (err, callback) => {
+    if (user === null) return console.error('User not found.');
+    User.comparePassword(password, (err, callback) => {
       if (err) return console.error('An error occurred while authenticating:', `\n${err}`);
-      if (callback === true) return console.log(`User '${user}' logged in succesfully`);
-    })
+      if (callback === true) return console.log(`User '${user}' logged in successfully`);
+    });
+    delete user.password;
+    callback(user, true);
   });
 }
 
-exports.newUser = (data) => {
+exports.newUser = (data, callback) => {
   var newUser = new User({
     firstName: data.firstName,
     lastName: data.lastName,
@@ -23,5 +28,16 @@ exports.newUser = (data) => {
 
   newUser.save((err) => {
     if (err) return console.error('An error occurred while creating a new user:', `\n${err}`);
+    callback(true);
+  });
+}
+
+exports.editUser = () => {
+
+}
+
+exports.deleteUser = (data, callback) => {
+  User.findByIdAndDelete(data, err => {
+    if (err) return console.error(`An error occurred while trying to delete a User: \n${err}`);
   });
 }
